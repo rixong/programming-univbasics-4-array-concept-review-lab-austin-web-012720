cef75f209fa3d08796b34099e40f0e2d91a19495
Rick Glascock <rixong@gmail.com>
2020-01-09 18:12:55 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,7 +5,13 @@
     return nil
   end
   
-
+  def find_max_value(array)
+    max = 0
+    array.length.times { |index|
+      max = array[index] if array[index] > max
+    }
+    return max
+  end
 
 
 
