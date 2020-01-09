59a463b68d389e3e86a53918b02b8b99940cfc27
Rick Glascock <rixong@gmail.com>
2020-01-09 18:17:57 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -6,15 +6,15 @@
   end
   
   def find_max_value(array)
-    max = 0
+    max = array[0]
     array.length.times { |index|
       max = array[index] if array[index] > max
     }
     return max
   end
 
-  def find_max_value(array)
-    max = 0
+  def find_min_value(array)
+    min = array[0]
     array.length.times { |index|
       max = array[index] if array[index] > max
     }
