58949f37121c980066e022e56b162c0d0de64d9d
Rick Glascock <rixong@gmail.com>
2020-01-09 17:57:09 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,11 @@
-def find_element_index(array, value_to_find)
-  array.length.times { |index|
-    return index if array[index] == value_to_find
-  }
-end
+  def find_element_index(array, value_to_find)
+    array.length.times { |index|
+      if array[index] == value_to_find
+        return index
+      else return nil
+      end
+    }
+  end
   
 
 
